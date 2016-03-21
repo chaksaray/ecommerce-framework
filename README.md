d7f2dd886a2512944d373a2c28829b98b3e539e9
chaksaray <chaksaray@gmail.com>
2016-03-21 13:22:59 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+ecommerce-framwork
+==============================
+
+package to add module products, point, member, and cart.
+
+Installation
+------------
+
+With Meteor >=0.9.0:
+
+```sh
+$ meteor add saraychak:ecommerce-framwork
+```
+
+Install like so:
+
+```sh
+$ meteor add saraychak:ecommerce-framwork
+```
+
+This package is a replacement for the official `ecommerce-framwork` package, so remove it if it's already in your project:
+
+```sh
+$ meteor remove saraychak:ecommerce-framwork
+```
+
+You will also can find at github.
+
+How to use
+----------
+
+Add `{{> ecommerceFramwork}}` to your body
+
+Example:
+
+```html
+<head>
+  <title>ecommerce-framwork</title>
+</head>
+
+<body>
+  {{> ecommerceFramwork}}
+</body>
+```
+Thanks.
---
+++ package.js
@@ -0,0 +1,26 @@
+Package.describe({
+  name: 'saraychak:ecommerce-framwork',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'package to add module products, point, member, and cart',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/chaksaray/ecommerce-framwork.git',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use('ecmascript');
+  api.use("templating", "client");
+  api.addFiles('ecommerce-framwork.js');
+  api.addFiles('ecommerce_framwork.html', 'client');
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('saraychak:ecommerce-framwork');
+  api.addFiles('ecommerce-framwork-tests.js');
+});
