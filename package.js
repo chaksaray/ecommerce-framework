12312bc3268c242e421392f29b00a34bd8936eb8
Chak Saray <saray.chak007@gmail.com>
2017-07-27 10:51:51 +0700

    Update package.js

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'saraychak:ecommerce-framwork',
+  name: 'saraychak:sample-meteor-package',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: 'package to add module products, point, member, and cart',
+  summary: 'This is the sample meteor package.',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/chaksaray/ecommerce-framework.git',
+  git: 'https://github.com/chaksaray/sample-meteor-package.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -14,13 +14,13 @@ Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
   api.use("templating", "client");
-  api.addFiles('ecommerce-framwork.js');
-  api.addFiles('ecommerce_framwork.html', 'client');
+  api.addFiles('sample-meteor-package.js');
+  api.addFiles('sample-meteor-package.html', 'client');
 });
 
 Package.onTest(function(api) {
   api.use('ecmascript');
   api.use('tinytest');
-  api.use('saraychak:ecommerce-framwork');
-  api.addFiles('ecommerce-framwork-tests.js');
+  api.use('saraychak:sample-meteor-package');
+  api.addFiles('sample-meteor-package.js');
 });
